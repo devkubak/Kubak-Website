34a2a981fc2008aaf9915f907dcba5e22b3cb7b7
RovarQRoom <rovar.07000120@outlook.com>
2023-12-20 15:05:10 +0300

    Update environment variables and repository names

--- src/lib/Env/env.environment.ts
+++ src/lib/Env/env.environment.ts
@@ -10,6 +10,9 @@ export const appwrite_project = ENVIROMENT.VITE_APPWRITE_PROJECT;
 export const appwrite_database = ENVIROMENT.VITE_APPWRITE_DATABASE_ID;
 
 // Appwrite Collections
-export const appwrite_collection_carousel = ENVIROMENT.VITE_APPWRITE_CAROUSEL_ID;
-export const appwrite_collection_development_process = ENVIROMENT.VITE_APPWRITE_DEVELOPMENT_PROCESS_ID;
-export const appwrite_collection_projects = ENVIROMENT.VITE_APPWRITE_PROJECTS_ID;
+export const appwrite_collection_carousel =
+  ENVIROMENT.VITE_APPWRITE_CAROUSEL_ID;
+export const appwrite_collection_development =
+  ENVIROMENT.VITE_APPWRITE_DEVELOPMENT_ID;
+export const appwrite_collection_projects =
+  ENVIROMENT.VITE_APPWRITE_PROJECTS_ID;
--- src/lib/Repositories/Implementation/Carousel.Repository.ts
+++ src/lib/Repositories/Implementation/Carousel.Repository.ts
@@ -1,29 +1,29 @@
-import { databases } from '$lib/Appwrite/appwrite';
-import { appwrite_database, appwrite_collection_carousel } from '$lib/Env/env.environment';
-import type { ICarouselRepository } from '$lib/Repositories/Interface/I.Carousel.repository';
-import type { Carousel } from '$lib/Models/Entities/Carousel.Entities.Model';
-import { Query } from 'appwrite';
+import { databases } from "$lib/Appwrite/appwrite";
+import {
+  appwrite_database,
+  appwrite_collection_carousel,
+} from "$lib/Env/env.environment";
+import type { ICarouselRepository } from "$lib/Repositories/Interface/I.Carousel.repository";
+import type { Carousel } from "$lib/Models/Entities/Carousel.Entities.Model";
+import { Query } from "appwrite";
 
 export class CarouselRepository implements ICarouselRepository {
-	async getCarousels(): Promise<AppwriteResponse<Carousel>> {
-		let { documents, total } = (await databases.listDocuments(
-			appwrite_database,
-			appwrite_collection_carousel,
-			[
-				Query.isNull("deletedAt"),
-				Query.limit(3)
-			  ]
-		)) as AppwriteResponse<Carousel>;
+  async getCarousels(): Promise<AppwriteResponse<Carousel>> {
+    let { documents, total } = (await databases.listDocuments(
+      appwrite_database,
+      appwrite_collection_carousel,
+      [Query.isNull("deletedAt"), Query.limit(3)]
+    )) as AppwriteResponse<Carousel>;
 
-		return { documents, total };
-	}
-	async getCarousel(id: string): Promise<Carousel> {
-		let document = (await databases.getDocument(
-			appwrite_database,
-			appwrite_collection_carousel,
-			id
-		)) as Carousel;
+    return { documents, total };
+  }
+  async getCarousel(id: string): Promise<Carousel> {
+    let document = (await databases.getDocument(
+      appwrite_database,
+      appwrite_collection_carousel,
+      id
+    )) as Carousel;
 
-		return document;
-	}
+    return document;
+  }
 }
