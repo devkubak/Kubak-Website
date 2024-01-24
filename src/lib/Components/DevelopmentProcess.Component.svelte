f29f6e6290092fb7fce667cad104c25bd3346e7e
Dana-Sarkawt <dana.code01@gmail.com>
2024-01-24 16:11:20 +0300

    Refactor DevelopmentProcess.Component.svelte to improve card selection and language handling

--- src/lib/Components/DevelopmentProcess.Component.svelte
+++ src/lib/Components/DevelopmentProcess.Component.svelte
@@ -1,104 +1,169 @@
 <script lang="ts">
   import type { Language } from "$lib/Models/Common/Language.Common.Model";
   import { developmentStore } from "$lib/Store/Development.Store";
-  import { Tabs, TabItem } from "flowbite-svelte";
   import { onMount } from "svelte";
   import { _, locale } from "svelte-i18n";
+
+  let selectedCard: number  = 0;
 
   onMount(async () => {
     try {
       await developmentStore.getAll();
       console.log($developmentStore);
+      selectedCard = $developmentStore.total % 4
     } catch (e) {
       console.log(e);
     }
   });
+
+  function handleCardClick(index: number) {
+    console.log(index);
+    if (selectedCard !== index) {
+     
+      console.log(selectedCard);
+      selectedCard = index
+    } else {
+      return;
+    }
+  }
+
   export let selectedLanguage: string;
-  function checkLanguage(
-    selectedLanguage: string,
-    text: Language
-  ): string {
+  function checkLanguage(selectedLanguage: string, text: Language): string {
     console.log(text);
     if (Object.keys(text).includes(selectedLanguage)) {
       console.log(text);
-      return text[
-        selectedLanguage as keyof typeof text
-      ] as string;
+  
+      return text[selectedLanguage as keyof typeof text] as string;
     } else {
       return text.en;
     }
-    
   }
 </script>
 
-{#if $locale == "en"}
 <div
-  class="container mx-auto w-full md:w-1/2 flex flex-col md:flex-row justify-center items-center h-auto bg-[#e3e3e3] dark:bg-[#212121] rounded-3xl p-5"
+  class="w-full h-auto justify-center items-center container mx-auto"
+  id="wrapper"
 >
-
-  <Tabs
-    style="pill"
-    class="flex"
-    defaultClass="flex flex-row md:flex-col-reverse gap-3 flex-wrap justify-center mr-4 mb-4 md:mb-0"
-    contentClass=" w-full h-auto min-h-[610px] bg-white dark:bg-[#363636] rounded-2xl flex items-start justify-center"
+  <div
+    class="h-auto gap-3 md:gap-0 md:h-auto flex flex-nowrap flex-col md:flex-row justify-center items-center md:items-stretch"
+    id="container"
+    dir={$locale == "en" ? "ltr" : "rtl"}
   >
-    {#each $developmentStore.data as development}
-      <TabItem
-        open
-        activeClasses="bg-white dark:bg-[#363636] w-full rounded-lg md:rounded-xl py-5 w-full px-4 text-[#f17f18] font-bold duration-300 ease-in-out"
-        inactiveClasses="hover:bg-[#f1f1f1] dark:hover:bg-[#404040] w-full rounded-lg md:rounded-xl py-5 w-full px-4  text-black dark:text-white font-bold duration-300 ease-in-out"
+    {#each $developmentStore.data as development, index}
+      <input
+        type="radio"
+        id={"c" + index}
+        name="card-group"
+        checked={index == $developmentStore.total % 4}
+        on:change={() => handleCardClick(index)}
+      />
+      <!-- svelte-ignore a11y-click-events-have-key-events -->
+      <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+      <label
+        class="{selectedCard === index
+          ? 'w-[80%] md:w-[80px] h-full'
+          : 'w-[80%] md:w-[80px] h-16 md:h-[600px]'} overflow-hidden object-cover cursor-pointer rounded-[2rem] my-0 mx-2 flex items-end"
+        for={"c" + index}
+        id="card"
+        style="background-image: url({development.image});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-filter: #212121;"
+        class:grayscale-filter={selectedCard !== index}
+        class:selected={selectedCard === index}
+        on:click={() => handleCardClick(index)}
       >
-        <span slot="title" class="text-sm">{checkLanguage(selectedLanguage, development.title)}</span>
         <div
-          class="flex flex-col w-full h-1/2 justify-center items-center p-4 gap-3"
+          class={`${selectedCard === index
+            ? 'bg-[#212121a9] h-auto'
+            : 'bg-[#212121a9] h-full '} py-8 px-4 flex justify-center items-center backdrop-blur-lg`}
+          id="row"
+          style="width: 100%;"
         >
-          <img
-            src={development.image}
-            class="rounded-2xl h-[350px] object-cover w-full"
-            alt=""
-          />
-          <p class="dark:text-white text-sm md:text-lg h-1/2 w-full">
-            {checkLanguage(selectedLanguage, development.description)}
-          </p>
+          <div
+            class="content-container font-bold "
+            id="description"
+            class:selected={selectedCard === index}
+          >
+            <h4 class="text-[#f17f18] text-lg">
+              {checkLanguage(selectedLanguage, development.title)}
+            </h4>
+            <p class="text-white pt-[5px] px-2">
+              {checkLanguage(selectedLanguage, development.description)}
+            </p>
+          </div>
         </div>
-      </TabItem>
+      </label>
     {/each}
-  </Tabs>
+  </div>
 </div>
 
-{:else}
+<style>
+  #card {
+    transition: 0.6s cubic-bezier(0.28, -0.03, 0, 0.99);
+    box-shadow: 0px 10px 30px -5px rgba(0, 0, 0, 0.8);
+  }
 
-<div
-  class="container mx-auto w-full md:w-1/2 flex flex-col md:flex-row-reverse justify-center items-center h-auto bg-[#e3e3e3] dark:bg-[#212121] rounded-3xl p-5"
->
+  #card > #row {
+    color: white;
+    display: flex;
+    flex-wrap: nowrap;
+  }
 
-  <Tabs
-    style="pill"
-    class="flex"
-    defaultClass="flex flex-row md:flex-col-reverse gap-3 flex-wrap justify-center ml-4 mb-4 md:mb-0"
-    contentClass=" w-full h-auto min-h-[610px] bg-white dark:bg-[#363636] rounded-2xl flex items-start justify-center"
-  >
-    {#each $developmentStore.data as development}
-      <TabItem
-        open
-        activeClasses="bg-white dark:bg-[#363636] w-full rounded-lg md:rounded-xl py-5 w-full px-4 text-[#f17f18] font-bold duration-300 ease-in-out"
-        inactiveClasses="hover:bg-[#f1f1f1] dark:hover:bg-[#404040] w-full rounded-lg md:rounded-xl py-5 w-full px-4  text-black dark:text-white font-bold duration-300 ease-in-out"
-      >
-        <span slot="title" class="text-sm">{checkLanguage(selectedLanguage, development.title)}</span>
-        <div
-          class="flex flex-col w-full h-1/2 justify-center items-center p-4 gap-3"
-        >
-          <img
-            src={development.image}
-            class="rounded-2xl h-[350px] object-cover w-full"
-            alt=""
-          />
-          <p class="dark:text-white text-sm md:text-lg h-1/2 w-full" dir="rtl">
-            {checkLanguage(selectedLanguage, development.description)}
-          </p>
-        </div>
-      </TabItem>
-    {/each}
-  </Tabs>
-</div>
-{/if}
+  #card > #row > #icon {
+    color: white;
+    border-radius: 50%;
+    width: 50px;
+    margin: 15px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .content-container {
+    opacity: 0;
+    transform: translateY(30px);
+    transition-delay: 0.3s;
+    transition: all 0.3s ease;
+  }
+
+  input:checked + label .content-container {
+    opacity: 1 !important;
+    transform: translateY(0px) !important;
+  }
+
+  input:checked + label #description {
+    opacity: 1 !important;
+    transform: translateY(0px) !important;
+  }
+
+  #description > h4 {
+    text-transform: uppercase;
+    margin-top: 13px;
+  }
+
+  input {
+    display: none;
+  }
+
+  label.selected {
+    width: 600px;
+  }
+  .grayscale-filter {
+    filter: opacity(70%);
+  }
+
+  input:checked + label#card {
+    width: 80%;
+    height: 600px;
+    filter: opacity(100%);
+  }
+
+  @media (max-width: 768px) {
+    label.selected {
+      height: 400px;
+      width: 80% !important;
+    }
+  }
+</style>
