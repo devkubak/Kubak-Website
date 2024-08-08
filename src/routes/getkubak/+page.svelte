231023f4c6478ee7a2018fc687b761c4c760a9ad
Dana-Sarkawt <dana.code01@gmail.com>
2024-08-08 13:59:48 +0300

    Fix bug in login functionality

---
+++ src/routes/getkubak/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+  
+  
+      onMount(() => {
+          redirectToStore();
+      });
+  function redirectToStore() {
+          var userAgent = navigator.userAgent || navigator.vendor;
+  
+          // Android
+          if (/android/i.test(userAgent)) {
+              window.location.href = 'https://play.google.com/store/apps/details?id=co.kubak.city&hl=en&gl=US';
+          }
+          // iOS
+          else if (/iPad|iPhone|iPod/.test(userAgent)) {
+              window.location.href = 'https://apps.apple.com/us/app/kubak/id1460687618';
+          }
+          // Other devices
+          else {
+              window.location.href = 'https://play.google.com/store/apps/details?id=co.kubak.city&hl=en&gl=US';
+          }
+          return {
+              redirectToStore
+          }
+      }
+  </script>
